440ca03b17751a007633309ec1d456f0636a4d38
Ankit <kashyap25ankit@gmail.com>
2024-12-31 00:12:26 +0530

    fixed the navbar bug

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import Link from "next/link";
 import { FaGithub } from "react-icons/fa";
 import navLinks from "@/lib/navlinks.json";
-import { cousine } from "@/public/font";
 
 export default function Navbar() {
   return (
@@ -17,13 +16,13 @@ export default function Navbar() {
         />
       </Link>
 
-      <div className="flex gap-6 border border-neutral-700 px-4 py-2 rounded-full text-sm bg-black">
+      <div className="flex gap-6 border border-neutral-700 px-4 py-2 rounded-full text-xs text-sm bg-black">
         {navLinks.map((e, i: number) => {
           return (
             <Link
               href={e.link}
               key={i}
-              className={`${cousine.className} hover:text-blue-500 cursor-pointer duration-500 `}
+              className={` hover:text-blue-500 cursor-pointer duration-500 `}
             >
               {e.name}
             </Link>
